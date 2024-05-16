0dc744f5bd3678302e812c02df47b32b2d550acd
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-15 22:10:46 -0400

    Finish of section 41.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 int num2 = 0;
 Console.Write("Please enter the first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -10,7 +9,7 @@ num2 = Convert.ToInt32(numberEntered);
 
 /*
     Math Operations and Operators
- */
+*/
 
 // add 
 int sum = num1 + num2;
@@ -24,16 +23,18 @@ int difference = num1 - num2;
 int mod = num1 % num2;
 
 Console.WriteLine("****************** MATH RESULTS ******************");
+Console.WriteLine();
 Console.WriteLine("Sum: " +sum);
 Console.WriteLine($"Product: {product}");
 Console.WriteLine($"Quotient: {quotient}");
 Console.WriteLine($"Difference: {difference}");
 Console.WriteLine($"Modulus: {mod}");
 Console.WriteLine("****************** END MATH RESULTS ******************");
+Console.WriteLine();
 
 /*
     Logic Operations and Operators
- */
+*/
 
 bool isGreaterThan = num1 > num2;
 bool isLessThan = num1 < num2;
@@ -43,6 +44,7 @@ bool isLessThanOrEqualTo = num1 <= num2;
 bool isNotEqual = num1 != num2;
 
 Console.WriteLine("****************** LOGIC RESULTS ******************");
+Console.WriteLine();
 Console.WriteLine($"Is Greater Than: {isGreaterThan}");
 Console.WriteLine($"Is Less Than: {isLessThan}");
 Console.WriteLine($"Is Equal to: {isEqualTo}");
@@ -50,3 +52,23 @@ Console.WriteLine($"Is Greater Than Or Equal to: {isGreaterThanOrEqualTo}");
 Console.WriteLine($"Is Less Than Or Equal to: {isLessThanOrEqualTo}");
 Console.WriteLine($"Is Not Equal: {isNotEqual}");
 Console.WriteLine("****************** END LOGIC RESULTS ******************");
+Console.WriteLine();
+
+/*
+    Assignment Operations and Operators
+*/
+
+Console.WriteLine("****************** ASSIGNMENT RESULTS ******************");
+Console.WriteLine();
+num1 += 5;
+Console.WriteLine($"Num 1 increased by 5: {num1}");
+num1 -= 3;
+Console.WriteLine($"Num 1 reduced by 3: {num1}");
+num1 /= 2;
+Console.WriteLine($"Num 1 divided by 2: {num1}");
+num1 %= 2;
+Console.WriteLine($"Num 1 mod by 2: {num1}");
+num1 *= 10;
+Console.WriteLine($"Num 1 multiplied by 10: {num1}");
+Console.WriteLine("****************** END ASSIGNMENT RESULTS ******************");
+Console.WriteLine();
